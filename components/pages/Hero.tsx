e986102d8902fa0836039c5eb88bba0c3c56770b
Muhammad alfarizi <alfakiddrock7@gmail.com>
2026-03-13 14:07:57 +0800

    update responsive

--- components/pages/Hero.tsx
+++ components/pages/Hero.tsx
@@ -11,41 +11,61 @@ const Hero = ({ scrollToSection }: HeroProps) => {
     return (
         <section
             id="home"
-            className="relative w-full h-screen flex flex-col items-center justify-between py-10"
+            className="relative w-full h-screen flex flex-col items-center justify-between py-6 md:py-8 lg:py-10"
         >
-            
-            {/* Konten utama di tengah */}
-            <div className="flex-1 flex flex-col items-center justify-center">
-                {/* Container untuk text PORTOFOLIO dan gambar */}
-                <div className="relative flex flex-col items-center justify-center">
-                    <span className="text-[13rem] font-body leading-none">PORTOFOLIO</span>
-                    
-                    {/* Gambar absolute di tengah text */}
-                    <div className="absolute inset-0 flex items-center justify-center">
-                        <Image 
-                            src="/alfa.png" 
-                            alt="Alfa Profile" 
-                            width={400}  // Sesuaikan dengan ukuran asli gambar
-                            height={400} // Sesuaikan dengan ukuran asli gambar
-                            className="w-auto h-auto"
-                            priority // Prioritas loading untuk LCP
-                            quality={90} // Optimasi kualitas
-                            loading="eager" // Langsung load tanpa lazy
-                        />
+
+            <div className="w-full flex-1 flex flex-col items-center justify-center">
+
+                {/* Container text + image */}
+                <div className="w-full relative flex inset-0 items-center justify-center">
+
+                    {/* TEXT PORTOFOLIO */}
+                    <span className="
+                        font-body leading-none
+                        text-[4rem] 
+                        sm:text-[5rem] 
+                        md:text-[8rem] 
+                        lg:text-[13rem]
+                    ">
+                        PORTOFOLIO
+                    </span>
+
+                    <div className="flex w-full absolute items-center justify-center">
+                        {/* IMAGE */}
+                        <div className="flex items-center justify-center">
+                            <Image
+                                src="/alfa.png"
+                                alt="Alfa Profile"
+                                width={400}
+                                height={400}
+                                priority
+                                quality={90}
+                                loading="eager"
+                            />
+                        </div>
+
+                        <div className="absolute -bottom-1 rotate-2 w-full flex justify-center items-center bg-black">
+                            <ScrollVelocity
+                                texts={['Web Developer', 'Creative Coder']}
+                                velocity={80}
+                                className="
+                            custom-scroll-text
+                            text-lg
+                            sm:text-2xl
+                            md:text-3xl
+                            lg:text-4xl
+                        "/>
+                        </div>
+
                     </div>
+
+
+
                 </div>
 
-                {/* Scroll text - tepat di bawah gambar */}
-                <div className="absolute bottom-1 rotate-2 w-full flex justify-center items-center bg-black py-2">
-                    <ScrollVelocity
-                        texts={['Web Developer', 'Creative Coder']}
-                        velocity={80}
-                        className="custom-scroll-text text-4xl"
-                    />
-                </div>
+
             </div>
 
-            {/* Optional: tambahkan elemen lain di bagian bawah jika diperlukan */}
         </section>
     );
 };
